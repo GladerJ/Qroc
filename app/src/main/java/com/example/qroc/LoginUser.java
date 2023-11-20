1b9a522a31a0d2d1ff03dc3de08c5fb3a75a4ae1
GladerJ <1417344239@qq.com>
2023-11-20 23:53:09 +0800

    页面跳转

---
+++ app/src/main/java/com/example/qroc/LoginUser.java
@@ -0,0 +1,43 @@
+package com.example.qroc;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginUser extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        /**从登录界面通过点击“注册用户”跳转到RegisterUser页面
+         *
+         */
+        TextView PrivacyPolicy = findViewById(R.id.register_user);
+
+        SpannableStringBuilder stringBuilder = new SpannableStringBuilder(PrivacyPolicy.getText());
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent intent = new Intent(LoginUser.this,RegisterUser.class);
+                startActivity(intent);
+
+            }
+
+        };
+        stringBuilder.setSpan(clickableSpan,0,4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        PrivacyPolicy.setMovementMethod(LinkMovementMethod.getInstance());
+        PrivacyPolicy.setHighlightColor(Color.TRANSPARENT);
+        PrivacyPolicy.setText(stringBuilder);
+    }
+}
